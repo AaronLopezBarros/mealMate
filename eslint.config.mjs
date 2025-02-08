3a00bce8d68012d32c6500d0dd219162295912d8
Aaron L <aaron.lb.dev@gmail.com>
2025-02-08 18:23:22 +0100

    chore: configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,46 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {},
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals"],
+    plugins: ["import"],
+    rules: {
+      "no-console": "error",
+      "no-unused-vars": "error",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            ["sibling", "parent"],
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+          ],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "newlines-between": "always",
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
