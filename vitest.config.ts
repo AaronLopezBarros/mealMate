567a3d3dee1bb6d4a444bf22da78f704ff2c6038
Aaron L <aaron.lb.dev@gmail.com>
2025-02-11 22:28:56 +0100

    chore: config vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,16 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './test/setup.ts',
+    coverage: {
+      all: true,
+      include: ['src/**/*.{ts,tsx,js,jsx}'],
+      exclude: ['node_modules', 'test/**/*'],
+      thresholds: {
+        lines: 80,
+        branches: 80,
+        functions: 80,
+        statements: 80,
+      },
+    },
   },
 });
