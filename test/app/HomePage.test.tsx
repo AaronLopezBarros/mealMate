6f44ba4b4a44077004fda8c76b5e81f55c234279
Aaron L <aaron.lb.dev@gmail.com>
2025-02-11 21:38:39 +0100

    test: test homepage

---
+++ test/app/HomePage.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+
+import Home from '../../src/app/page';
+
+describe('HomePage', () => {
+  it('should render corretly when it is mounted', () => {
+    render(<Home />);
+    expect(screen.getByText('Hello')).toBeInTheDocument();
+  });
+});
