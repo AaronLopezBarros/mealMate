edaddb7f5f05fed353c64b4e509e1d18f925269a
Aaron L <aaron.lb.dev@gmail.com>
2025-03-03 21:40:12 +0100

    feat: create quickly navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import { Calendar } from '@/components/Calendar';
+import { Navbar } from '@/components/Navbar';
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <Calendar />
+    </div>
+  );
 }
