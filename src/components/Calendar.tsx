7565ed6eb1427420b0ee7f26b694e12eed21dbe4
Aaron L <aaron.lb.dev@gmail.com>
2025-03-05 11:32:39 +0100

    feat: create calendar component

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,20 @@
+const DAYS_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 export const Calendar = () => {
+  const twoWeeks = [...DAYS_OF_WEEK, ...DAYS_OF_WEEK];
   return (
-    <div className='bg-primary m-6 h-[600px] w-[600px] rounded-xl border-2 border-black p-6 shadow-2xl'>Calendar</div>
+    <section className='bg-primary m-6 max-w-[700px] rounded-xl border-2 border-black p-6 shadow-2xl'>
+      <h2 className='text-center text-2xl font-bold'>Calendar</h2>
+      <div className='mt-6 flex flex-wrap gap-2'>
+        {twoWeeks.map((day, index) => (
+          <div
+            key={index}
+            className='border-1 bg-background-white h-[150px] w-[150px] rounded-xl border border-black p-2 text-center shadow-xl'
+          >
+            {day}
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
