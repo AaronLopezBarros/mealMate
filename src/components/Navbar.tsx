91c8e4d5c782883a04224cdde493a60efc18e6aa
Aaron L <aaron.lb.dev@gmail.com>
2025-03-05 11:32:18 +0100

    feat: create quickly navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 export const Navbar = () => {
   return (
     <div className='bg-primary flex items-center justify-between border-b border-black p-4 text-black'>
-      <div className='text-2xl font-bold'>MealMate!</div>
+      <h1 className='text-3xl font-bold'>MealMate</h1>
       <div className='space-x-4'>
-        <a href='#' className='hover:text-gray-300'>
+        <a href='#' className='hover:underline'>
           Home
         </a>
-        <a href='#' className='hover:text-gray-300'>
+        <a href='#' className='hover:underline'>
           About
         </a>
-        <a href='#' className='hover:text-gray-300'>
+        <a href='#' className='hover:underline'>
           Contact
         </a>
       </div>
